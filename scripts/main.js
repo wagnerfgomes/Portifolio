f40b1bcd1ac4ad6fa80e8574d12275d031190981
Wagner Felipe <wagnerfelipe4545@gmail.com>
2024-09-05 01:14:48 -0300

    primeiro commit (tentativa 2)

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let menuMobile = document.querySelector('#menuMobile');
+let imgMenuMobile = document.querySelector('.menu-mobile__button__img');
+function menuShow() {
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.menu-mobile__button__img').src =
+            '/assets/menuHambuger.svg';
+    } else{
+        menuMobile.classList.add('open');
+        document.querySelector('.menu-mobile__button__img').src =
+            '/assets/menuHambugerclose.svg';
+    }
+}
