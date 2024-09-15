648ae8149d6ba22e538cd2c2ddc44103bffc0d33
Wagner Felipe <wagnerfelipe4545@gmail.com>
2024-09-14 23:31:29 -0300

    redesign do menu mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 const menuMobile = document.querySelector('#menuMobile');
 const imgMenuMobile = document.querySelector('#icon-button-menu');
+
+const menuMobileButton = document.querySelector('#button-menu-mobile');
+
+menuMobileButton.addEventListener('click', menuShow);
+
 function menuShow() {
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
