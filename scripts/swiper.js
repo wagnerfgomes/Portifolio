5a1462862d93aeb0d7729f9e851e0aad26f88116
Wagner Felipe <wagnerfelipe4545@gmail.com>
2024-09-26 01:34:05 -0300

    o slider exite

---
+++ scripts/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  freeMode: true,
+  // centeredSlides: true,
+  spaceBetween: 100,
+  pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+  },
+  slidesPerView: 2,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  // speed: 200, 
+});
